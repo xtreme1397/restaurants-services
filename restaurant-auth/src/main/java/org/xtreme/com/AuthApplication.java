53c45ca4633cc68c6a440298f79f8831970dd4a9
Abhishek Kumar <abhics2410@gmail.com>
2018-08-20 11:12:54 +0530

    Update AuthApplication.java

--- restaurant-auth/src/main/java/org/xtreme/com/AuthApplication.java
+++ restaurant-auth/src/main/java/org/xtreme/com/AuthApplication.java
@@ -22,8 +22,6 @@ public class AuthApplication {
 		Properties prop = new Properties();
 		try {
 			prop.load(new FileInputStream("common.properties"));
-			System.setProperty("application.instanceId", "cape-build-server.southeastasia.cloudapp.azure.com:studio:1");
-			System.setProperty("err.log", prop.getProperty("cape.error.logs-dir") + "/auth.err");
 		} catch (IOException e) {
 			throw new ServiceException("AuthApplication IOExecption while loading properties", e);
 		}
